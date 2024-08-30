6a9e2e7d3486f9d7702078ef358774349eaf9192
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-08-30 12:15:44 +0000

    Music Label Wireframes done

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.email "seydiahmedelcheikh@gmail.com"
 git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
-git commit -m "Card Sorting done"
+git commit -m "Music Label Wireframes done"
 git push
