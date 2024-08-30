efe51e99ffb1443a3af857dcbb362fac874adde7
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-08-30 13:00:41 +0000

    All is done

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.email "seydiahmedelcheikh@gmail.com"
 git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
-git commit -m "Run5Tests done"
+git commit -m "All is done"
 git push
