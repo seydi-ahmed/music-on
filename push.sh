964beec1976b05af01a6a5ab56a97aea1bf4d3f3
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-08-30 11:46:46 +0000

    Card Sorting done

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.email "seydiahmedelcheikh@gmail.com"
 git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
-git commit -m "JTBD done"
+git commit -m "Card Sorting done"
 git push
