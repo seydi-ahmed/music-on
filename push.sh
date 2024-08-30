ec530c26f558866cc64d59087b878a36468bd97d
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-08-30 11:36:37 +0000

    JTBD done

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.email "seydiahmedelcheikh@gmail.com"
 git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
-git commit -m "Site Map done"
+git commit -m "JTBD done"
 git push
